7eee4b6d06d9a00e6c44b3f33e53466afe9fa779
LOYKIKA <janakiramloykika8005@gmail.com>
2026-09-05 08:36:28 +0530

    Session 4

--- Q3.java
+++ Q3.java
@@ -1,30 +1,34 @@
-class Q3 {
-    String empId;
-    String empName;
-    double salary;
-    boolean isIntern;
+class ParkingTicket {
+    String vehicleNo;
+    double ratePerMinute;
 
-    public Employee(String empId, String empName, double salary) {
-        this.empId = empId;
-        this.empName = empName;
-        this.salary = salary;
-        this.isIntern = false;
+    public ParkingTicket(String vehicleNo, double ratePerMinute) {
+        this.vehicleNo = vehicleNo;
+        this.ratePerMinute = ratePerMinute;
     }
 
-    public Employee(String empId, String empName) {
-        this(empId, empName, 0.0);
-        this.isIntern = true;
+    public final double calculateFine(int overstayMinutes) {
+        return overstayMinutes * ratePerMinute;
     }
 
-    public void printProfile() {
-        System.out.println(empId + " | " + empName + " | Rs " + salary + " | Intern: " + isIntern);
+    public final void printReceipt(int overstayMinutes) {
+        System.out.println(vehicleNo + " - Fine: Rs " + calculateFine(overstayMinutes));
     }
+}
 
+public class Q3 {
     public static void main(String[] args) {
-        Employee emp1 = new Employee("E-101", "Divya", 65000);
-        Employee emp2 = new Employee("E-102", "Arjun");
+        String[] vehicleNos = {"TN09AB1234", "TN22CD5678", "TN09EF9012", "TN10GH3456"};
+        double[] ratePerMinute = {2.0, 2.0, 3.0, 2.0};
+        int[] overstayMinutes = {15, 0, -5, 8};
 
-        emp1.printProfile();
-        emp2.printProfile();
+        for (int i = 0; i < vehicleNos.length; i++) {
+            ParkingTicket ticket = new ParkingTicket(vehicleNos[i], ratePerMinute[i]);
+            if (overstayMinutes[i] > 0) {
+                ticket.printReceipt(overstayMinutes[i]);
+            } else {
+                System.out.println(vehicleNos[i] + " - No fine, within allotted time");
+            }
+        }
     }
 }
